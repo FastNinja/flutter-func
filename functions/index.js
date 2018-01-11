f1db7d70fe62f65fd8ed30bbce52914b9c00c1c7
Moh Rezaee <Moh.Rezaee@myob.com>
2018-01-11 21:27:29 +1100

    Added push notification function

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require("firebase-functions");
 const algoliasearch = require("algoliasearch");
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
 // [START init_algolia]
 // Initialize Algolia, requires installing Algolia dependencies:
@@ -40,6 +42,63 @@ exports.onMessageCreated = functions.database
     );
   });
 
+exports.sendPushNotification = functions.database
+    .ref("/jobMessages/{jobId}/{messageId}")
+    .onWrite(event => {
+      const message = event.data.val();
+      const userId = message.createdByUid;
+      console.log('We have a new notification for user: ', userId);
+
+      // Get the list of device notification tokens.
+      const getDeviceTokensPromise = admin.database().ref(`/users/${userId}/tokens`).once('value');
+
+      // Get the user profile.
+      const getMessagePromise = message.text;
+
+      return Promise.all([getDeviceTokensPromise, getMessagePromise]).then(results => {
+        const tokensSnapshot = results[0];
+        const txt = results[1];
+
+      // Check if there are any device tokens.
+      if (!tokensSnapshot.hasChildren()) {
+        return console.log('There are no notification tokens to send to.');
+      }
+
+      console.log('There are', tokensSnapshot.numChildren(), 'tokens to send notifications to.');
+      console.log('Fetched text ', txt);
+
+      // Notification details.
+      const payload = {
+          notification: {
+              title: 'Notification:',
+              body: txt
+          }
+      };
+
+      // Listing all tokens.
+      const tokens = Object.keys(tokensSnapshot.val());
+
+      // Send notifications to all tokens.
+      return admin.messaging().sendToDevice(tokens, payload).then(response => {
+        // For each message check if there was an error.
+        const tokensToRemove = [];
+        response.results.forEach((result, index) => {
+          const error = result.error;
+          if (error) {
+            console.error('Failure sending notification to', tokens[index], error);
+            // Cleanup the tokens who are not registered anymore.
+            if (error.code === 'messaging/invalid-registration-token' ||
+                error.code === 'messaging/registration-token-not-registered') {
+                tokensToRemove.push(tokensSnapshot.ref.child(tokens[index]).remove());
+            }
+          }
+        });
+
+        return Promise.all(tokensToRemove);
+      });
+    });
+  });
+
 function toEpochDateTime(utcDateTime) {
   // Algolia required Date time stamp to be in Epoch.
   return Math.floor(new Date(utcDateTime).getTime() / 1000);
@@ -62,10 +121,7 @@ function toEpochDateTime(utcDateTime) {
 //   });
 // // [END update_index_function]
 
-// [START get_firebase_user]
-const admin = require("firebase-admin");
-
-admin.initializeApp(functions.config().firebase);
+// [START get_firebase_user];
 
 function getFirebaseUser(req, res, next) {
   console.log("Check if request is authorized with Firebase ID token");
