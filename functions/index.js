53690dec5fe5ca7bbea50ec0583e903daf82c967
Moh Rezaee <Moh.Rezaee@myob.com>
2018-01-15 22:24:13 +1100

    Push Notification function completed

--- functions/index.js
+++ functions/index.js
@@ -45,57 +45,66 @@ exports.onMessageCreated = functions.database
 exports.sendPushNotification = functions.database
     .ref("/jobMessages/{jobId}/{messageId}")
     .onWrite(event => {
-      const message = event.data.val();
-      const userId = message.createdByUid;
-      console.log('We have a new notification for user: ', userId);
 
-      // Get the list of device notification tokens.
-      const getDeviceTokensPromise = admin.database().ref(`/users/${userId}/notificationTokens`).once('value');
 
-      // Get the user profile.
-      const getMessagePromise = message.text;
+    admin.database().ref("/users").once('value', function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
 
-      return Promise.all([getDeviceTokensPromise, getMessagePromise]).then(results => {
-        const tokensSnapshot = results[0];
-        const txt = results[1];
+            const message = event.data.val();
+            console.log("user is: *** : ", childSnapshot.key);
 
-      // Check if there are any device tokens.
-      if (!tokensSnapshot.hasChildren()) {
-        return console.log('There are no notification tokens to send to.');
-      }
+            const userId = childSnapshot.key;
+            console.log('We have a new notification for user: ', userId);
 
-      console.log('There are', tokensSnapshot.numChildren(), 'tokens to send notifications to.');
-      console.log('Fetched text ', txt);
 
-      // Notification details.
-      const payload = {
-          notification: {
-              title: 'Notification:',
-              body: txt
-          }
-      };
+              const getDeviceTokensPromise = admin.database().ref(`/users/${userId}/notificationTokens`).once('value');
 
-      // Listing all tokens.
-      const tokens = Object.keys(tokensSnapshot.val());
+              // Get the user profile.
+              const getMessagePromise = message.text;
 
-      // Send notifications to all tokens.
-      return admin.messaging().sendToDevice(tokens, payload).then(response => {
-        // For each message check if there was an error.
-        const tokensToRemove = [];
-        response.results.forEach((result, index) => {
-          const error = result.error;
-          if (error) {
-            console.error('Failure sending notification to', tokens[index], error);
-            // Cleanup the tokens who are not registered anymore.
-            if (error.code === 'messaging/invalid-registration-token' ||
-                error.code === 'messaging/registration-token-not-registered') {
-                tokensToRemove.push(tokensSnapshot.ref.child(tokens[index]).remove());
-            }
-          }
+              return Promise.all([getDeviceTokensPromise, getMessagePromise]).then(results => {
+                const tokensSnapshot = results[0];
+                const txt = results[1];
+
+                // Check if there are any device tokens.
+                if (!tokensSnapshot.hasChildren()) {
+                    return console.log('There are no notification tokens to send to.');
+                }
+
+              console.log('There are', tokensSnapshot.numChildren(), 'tokens to send notifications to.');
+              console.log('Fetched text ', txt);
+
+              // Notification details.
+              const payload = {
+                  notification: {
+                      title: 'Notification:',
+                      body: txt
+                  }
+              };
+
+              // Listing all tokens.
+              const tokens = Object.keys(tokensSnapshot.val());
+
+              // Send notifications to all tokens.
+              return admin.messaging().sendToDevice(tokens, payload).then(response => {
+                // For each message check if there was an error.
+                const tokensToRemove = [];
+                response.results.forEach((result, index) => {
+                  const error = result.error;
+                  if (error) {
+                    console.error('Failure sending notification to', tokens[index], error);
+                    // Cleanup the tokens who are not registered anymore.
+                    if (error.code === 'messaging/invalid-registration-token' ||
+                        error.code === 'messaging/registration-token-not-registered') {
+                        tokensToRemove.push(tokensSnapshot.ref.child(tokens[index]).remove());
+                    }
+                  }
+                });
+
+                return Promise.all(tokensToRemove);
+                });
+            });
         });
-
-        return Promise.all(tokensToRemove);
-      });
     });
   });
 
